1c797810abe8ec7b4ace5b0df483b5d41a5756aa
Saravanan S S <saravanansunisree@gmail.com>
2026-03-08 17:00:02 +0530

    Added PWA and mobile support

---
+++ frontend/script.js
@@ -0,0 +1,69 @@
+let symptoms = []
+
+function addSymptom(){
+
+    let input = document.getElementById("symptomInput")
+    let symptom = input.value.trim().toLowerCase()
+
+    if(symptom === "") return
+
+    symptoms.push(symptom)
+
+    let li = document.createElement("li")
+    li.innerText = symptom
+
+    document.getElementById("symptomList").appendChild(li)
+
+    input.value=""
+}
+
+async function predict(){
+
+    if(symptoms.length === 0){
+        alert("Please add symptoms first")
+        return
+    }
+
+    document.getElementById("disease").innerText="Predicting..."
+    document.getElementById("confidence").innerText="-"
+    document.getElementById("referral").innerText="-"
+
+    try{
+
+        let response = await fetch("https://caremind-ai.onrender.com/predict",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                symptoms:symptoms
+            })
+        })
+
+        if(!response.ok){
+            throw new Error("Server response error")
+        }
+
+        let data = await response.json()
+
+        document.getElementById("disease").innerText =
+        data.predicted_disease
+
+        document.getElementById("confidence").innerText =
+        (data.confidence_score * 100).toFixed(2) + "%"
+
+        document.getElementById("referral").innerText =
+        data.referral_decision
+
+        let bar = document.getElementById("confidenceFill")
+
+        bar.style.width = (data.confidence_score * 100) + "%"
+
+    }catch(error){
+
+        console.log(error)
+
+        alert("Error connecting to server")
+
+    }
+}
